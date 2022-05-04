64b622e47e55acd09ca79d377b0d0c172af7884b
RuanDevJs <ruanv@speediomail.com>
2022-05-03 22:52:03 -0300

    Updates for UI

--- App.js
+++ App.js
@@ -26,14 +26,34 @@ import {
 const { width, height } = Dimensions.get('screen');
 
 const data = [
-    'https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200',
-    'https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200',
-    'https://cdn.dribbble.com/users/3281732/screenshots/9165292/media/ccbfbce040e1941972dbc6a378c35e98.jpg?compress=1&resize=1200x1200',
-    'https://cdn.dribbble.com/users/3281732/screenshots/11205211/media/44c854b0a6e381340fbefe276e03e8e4.jpg?compress=1&resize=1200x1200',
-    'https://cdn.dribbble.com/users/3281732/screenshots/7003560/media/48d5ac3503d204751a2890ba82cc42ad.jpg?compress=1&resize=1200x1200',
-    'https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200',
-    'https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200'
-
+    {
+      url: 'https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200',
+      title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+      url: 'https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200',
+      title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+      url: 'https://cdn.dribbble.com/users/3281732/screenshots/9165292/media/ccbfbce040e1941972dbc6a378c35e98.jpg?compress=1&resize=1200x1200',
+      title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+      url: 'https://cdn.dribbble.com/users/3281732/screenshots/11205211/media/44c854b0a6e381340fbefe276e03e8e4.jpg?compress=1&resize=1200x1200',
+      title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+      url: 'https://cdn.dribbble.com/users/3281732/screenshots/7003560/media/48d5ac3503d204751a2890ba82cc42ad.jpg?compress=1&resize=1200x1200',
+      title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+      url: 'https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200',
+      title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    },
+    {
+      url: 'https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200',
+      title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    }
 ];
 
 const imageW = width * 0.7;
@@ -41,69 +61,82 @@ const imageH = imageW * 1.54;
 
 export default function App(){
   const scrollX = useRef(new Animated.Value(0)).current;
-  
+
   return(
     <View style={{ flex: 1, backgroundColor: '#000' }}>
-      <Animated.View 
-        style={[
-          StyleSheet.absoluteFillObject
-        ]}
-      >
-        { data.map((image, index) => {
-
-          const inputRange = [
-            (index - 1) * width,
-            index * width,
-            (index + 1) * width
-          ]; 
-
-          const opacity = scrollX.interpolate({
-            inputRange,
-            outputRange: [0, 1, 0]
-          })
+      <View style={[ StyleSheet.absoluteFillObject ]}>
+        {data.map((el, index) => {
+          const inputRange = [(index - 1) * width, index * width, (index + 1) * width]
 
           return(
             <Animated.Image
-              key={`Index: ${index}`}
-              source={{ uri: image }}
-              style={[
+              key={index * 3}
+              source={{ uri: el.url }}
+              style={[ 
                 StyleSheet.absoluteFillObject,
                 {
-                  opacity
+                  opacity: scrollX.interpolate({
+                    inputRange,
+                    outputRange: [0, 1, 0]
+                  })
                 }
-              ]}
+               ]}
               blurRadius={50}
             />
           )
         })}
-      </Animated.View>
+      </View>
       <Animated.FlatList
-        onScroll={Animated.event([{nativeEvent: {contentOffset: {x: scrollX }}}], {
-          useNativeDriver: true
-        })}
         data={data}
-        keyExtractor={((_, index) => index)}
+        onScroll={Animated.event([
+          {
+            nativeEvent: {
+              contentOffset: {
+                x: scrollX
+              }
+            }
+          }
+        ], {
+          useNativeDriver: false
+        })}
+        keyExtractor={(((_, index) => index))}
         horizontal
         pagingEnabled
-        renderItem={(({ item }) => {
+        scrollEventThrottle={16}
+        renderItem={(({ item, index }) => {
           return(
             <View 
-              style={{
-                flex: 1,
-                width: width,
-                justifyContent: 'center',
+              style={{ 
+                justifyContent: 'center', 
                 alignItems: 'center',
+                width
               }}
             >
               <Image
-                  source={{ uri: item }}
-                  style={{ 
-                    width: imageW,
-                    height: imageH,
-                    resizeMode: 'cover',
-                    borderRadius: 16
-                  }}
+                source={{ uri: item.url }}
+                style={{
+                  width: imageW,
+                  height: imageH,
+                  resizeMode: 'cover',
+                  borderRadius: 16
+                }}
               />
+              <Animated.Text 
+                style={{ 
+                  width: 360,
+                  fontSize: scrollX.interpolate({
+                    inputRange: [(index - 1) * width, index * width, (index + 1) * width],
+                    outputRange: [12, 24, 12],
+                    extrapolate: 'clamp'
+                  }),
+                  color: '#f2f2f2',
+                  fontWeight: '500',
+                  textAlign: 'center',
+                  marginTop: 32
+                }}
+              >
+                Desenvolvido por Ruan Vitor
+              </Animated.Text>
             </View>
           )
         })}
